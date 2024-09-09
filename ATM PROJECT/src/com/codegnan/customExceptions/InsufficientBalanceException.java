b09cff20d60588ccb21287754567b5de4469066f
nanmaria <mn472246@gmail.com>
2024-09-09 13:06:19 +0530

    Intial commit

---
+++ ATM PROJECT/src/com/codegnan/customExceptions/InsufficientBalanceException.java
@@ -0,0 +1,10 @@
+package com.codegnan.customExceptions;
+
+	public class InsufficientBalanceException extends Exception {
+	 	public InsufficientBalanceException(String errorMsg) {
+	      	super(errorMsg);
+	 	}
+	 
+	}
+
+
